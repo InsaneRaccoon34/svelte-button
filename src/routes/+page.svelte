208cbb395b717b9111f69c145dc59f374ba61700
andy <morozuk44@gmail.com>
2024-02-10 17:59:13 +0200

    init, initial button

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<section class="container">
+	<a class="button">This Is A BUTTON</a>
+</section>
+
+<style lang="scss">
+	.container {
+    height: 80vh;
+    width: 100%;
+    display: grid;
+    place-items: center;
+	}
+
+  .button {
+    // container
+    padding: 1rem 1rem;
+    outline: $orange 2px solid;
+    border-radius: 2px;
+
+    // text
+    color: $orange;
+
+    // behaviour
+    transition: all 50ms ease;
+  }
+</style>
