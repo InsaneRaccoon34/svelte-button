d8b2d1451c6eb9cb51374e85fa99e3e6b87172d0
andy <morozuk44@gmail.com>
2024-02-10 18:45:07 +0200

    feat: added animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,27 +2,58 @@
 </script>
 
 <section class="container">
-	<a class="button">This Is A BUTTON</a>
+	<a class="button"> This Is A BUTTON </a>
 </section>
 
 <style lang="scss">
+	$border-radius: 0.5rem;
+	$text-color: black;
+
 	.container {
-    height: 80vh;
-    width: 100%;
-    display: grid;
-    place-items: center;
+		height: 80vh;
+		width: 100%;
+		display: grid;
+		place-items: center;
 	}
 
-  .button {
-    // container
-    padding: 1rem 1rem;
-    outline: $orange 2px solid;
-    border-radius: 2px;
+	.button {
+		position: relative;
+		padding: 3rem 5rem;
+		outline: $orange 2px solid;
+		border-radius: $border-radius;
+		font-size: 2rem;
+		cursor: pointer;
 
-    // text
-    color: $orange;
+		color: $orange;
+		transition: 0.3s;
+		transition-property: color transform;
 
-    // behaviour
-    transition: all 50ms ease;
-  }
+		&::after {
+			content: '';
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			border-radius: $border-radius;
+
+			background-color: #f5900b;
+
+			height: 0;
+			width: 100%;
+
+			z-index: -1;
+
+			transition: all 0.5s;
+		}
+
+		&:hover {
+			color: white;
+			transform: scale(1.1);
+      box-shadow: 10px 10px 8px -4px rgba(0, 0, 0, 0.75);
+			&::after {
+				border-radius: $border-radius;
+				height: 100%;
+				width: 100%;
+			}
+		}
+	}
 </style>
